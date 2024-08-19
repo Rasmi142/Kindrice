d951843589fafbe86170b8bfeaa414f880ae2247
Rasmi142 <rasmioff@gmail.com>
2024-08-19 17:50:06 +0530

    hero-section modified

--- frontend/src/Components/Hero-section/Herosection.jsx
+++ frontend/src/Components/Hero-section/Herosection.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import { assets } from '../../assets/assets';
 import './Herosection.css';
 
 export default function Herosection() {
+  const navigate = useNavigate(); 
+  
+
+  const handleBuyNowClick = () => {
+    navigate('/shop'); 
+  };
+
   return (
     <section className='hero-section'>
       <div className="background-outer-layer">
         <div className="inner-layer">
           <div className="assets">
-          <img src={assets.outdoor} alt=''/>
+            <img src={assets.outdoor} alt='' />
           </div>
           <div className="content-overlay">
             <h1>Kind low-GI Rice
-                <br/>
+                <br />
                 So Clean. So Tasty. So Healthy.
             </h1>
+            <p>From ₹210<br />
+            <span onClick={handleBuyNowClick}>Buy Now</span></p>
           </div>
         </div>
-       
       </div>
       <div className='second-section'>
-        <img src={assets.Home2} alt='home2'/>
+        <img src={assets.Home2} alt='home2' />
       </div>
     </section>
   );
